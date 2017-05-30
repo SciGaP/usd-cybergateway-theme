a1f12431b654d528859fe9649d5af1b26a042b99
Wendi <wendikristine@gmail.com>
2017-05-30 09:26:56 -0500

    Update template.blade.php

--- partials/template.blade.php
+++ partials/template.blade.php
@@ -1,4 +1,11 @@
-
+ 
+<div class="col-md-12 hero-unit scigap-top">
+	
+	<!-- <h1>Hero Image - WHERE?</h1> -->
+	<!-- <h2>This is a simple hero unit, a simple jumbotron-style component for calling extra 7  -->
+	<!-- attention to featured content or information.</h2>  -->
+	
+</div>
 
 <div class="col-md-offset-2 col-md-8 breathing-space scigap-info">
 	<h1 class="text-center">Welcome to research computing!!</h1>
@@ -13,10 +20,3 @@
 </div>	
 
 
-<div class="col-md-12 hero-unit scigap-top">
-	
-	<!-- <h1>Hero Image - WHERE?</h1> -->
-	<!-- <h2>This is a simple hero unit, a simple jumbotron-style component for calling extra 7  -->
-	<!-- attention to featured content or information.</h2>  -->
-	
-</div>
