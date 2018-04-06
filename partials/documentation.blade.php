89c9722b99b5040399db1c55ee6d244cd22a6b7f
Joe Madison <Joseph.Madison@coyotes.usd.edu>
2018-04-06 16:01:40 -0500

    Create documentation.blade.php

---
+++ partials/documentation.blade.php
@@ -0,0 +1,18 @@
+div class="container">       
+				
+                
+<h1 class="text-center top-space">About Science Gateway at USD</h1>
+
+                 
+<div class="post-entry">
+<p dir="ltr">Science Gateways are virtual environments that accelerate scientific discovery by enabling scientific communities to utilize distributed computational and data resources (that is, cyberinfrastructure). Successful Science Gateways provide access to sophisticated and powerful resources, while shielding their users from the resources’ complexities.</p>
+
+<p dir="ltr">Research Computing at USD wants you to be able to focus on the science. Let us worry about cyberinfrastructure.</p>
+
+</div><!-- end of .post-entry -->
+            
+				
+
+<div class="post-edit"></div>  				               
+				      
+</div>
