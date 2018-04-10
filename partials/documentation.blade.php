f1e86dfbb5097d365af88f48879e4b1ef46e91cb
Joe Madison <Joseph.Madison@coyotes.usd.edu>
2018-04-09 22:20:47 -0500

    Update documentation.blade.php

--- partials/documentation.blade.php
+++ partials/documentation.blade.php
@@ -7,7 +7,7 @@ div class="container">
 <div class="post-entry">
 <p dir="ltr">Science Gateways are virtual environments that accelerate scientific discovery by enabling scientific communities to utilize distributed computational and data resources (that is, cyberinfrastructure). Successful Science Gateways provide access to sophisticated and powerful resources, while shielding their users from the resources’ complexities.</p>
 
-<p dir="ltr">Research Computing at USD wants you to be able to focus on the science. Let us worry about cyberinfrastructure.</p>
+<p dir="ltr">Research Computing at USD wants you to be able to focus on the science. This gateway will allow users to experience the benefits of large computational resources for analyzing their data in a user friendly environment.</p>
 
 </div><!-- end of .post-entry -->
             
