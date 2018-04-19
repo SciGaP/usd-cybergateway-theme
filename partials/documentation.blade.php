b44bf6d89e70ca2cd8a8f6e373a7edd094e7b095
Joe Madison <Joseph.Madison@coyotes.usd.edu>
2018-04-19 09:06:20 -0500

    Update documentation.blade.php

--- partials/documentation.blade.php
+++ partials/documentation.blade.php
@@ -7,6 +7,8 @@ div class="container">
 <div class="post-entry">
 <p dir="ltr">Here you will find the documentation necessary for getting started on the USD Science Gateway. The Science Gateway currently supports the submission of computational tasks with the software in widest use by USD faculty and staff.</p>
 
+<p dir="ltr">The USD Science Gateway currently supports the folowing software applications for use on the Lawrence cluter: </p>
+
 </div><!-- end of .post-entry -->
             
 				
