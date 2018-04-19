ed0836ba6582a235d39bbaad47f01d76ee419ec0
Joe Madison <Joseph.Madison@coyotes.usd.edu>
2018-04-19 09:37:04 -0500

    Update documentation.blade.php

--- partials/documentation.blade.php
+++ partials/documentation.blade.php
@@ -7,7 +7,13 @@
 <div class="post-entry">
 <p dir="ltr">Here you will find the documentation necessary for getting started on the USD Science Gateway. The Science Gateway currently supports the submission of computational tasks with the software in widest use by USD faculty and staff.</p>
 
-<p dir="ltr">The USD Science Gateway currently supports the folowing software applications for use on the Lawrence cluter: </p>
+<p dir="ltr">The USD Science Gateway currently supports the following software applications for use on the Lawrence cluter: </p>
+
+<p dir="ltr">Mothur-Used for analysis of microbial DNA sequence datasets.
+
+<p dir="ltr">NWChem
+
+<p dir="ltr">Gaussian
 
 </div><!-- end of .post-entry -->
             
